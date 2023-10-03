ffbb4b595ea8b424d759c2ee2a45bb7cb16a8730
Justin De Burgo <justin.deburgo@aa.com>
2023-10-03 14:42:45 -0500

    adding tests
    Signed-off-by: Justin De Burgo <justin.deburgo@aa.com>

--- src/components/ExampleComponent/ExampleComponent.test.tsx
+++ src/components/ExampleComponent/ExampleComponent.test.tsx
@@ -6,17 +6,6 @@ import { screen } from '@testing-library/react';
 import { setupRequestMockHandlers, renderInTestApp } from '@backstage/test-utils';
 
 describe('ExampleComponent', () => {
-  const server = setupServer();
-  // Enable sane handlers for network requests
-  beforeAll(() => {
-    setupRequestMockHandlers(server);
-  });
-
-  // setup mock response
-  beforeEach(() => {
-    server.use(rest.get('/*', (_, res, ctx) => res(ctx.status(200), ctx.json({}))));
-  });
-
   it('renders example component', async () => {
     await renderInTestApp(<ExampleComponent />);
     expect(screen.getByText('Welcome to tamu-fall-2023-frontend!')).toBeInTheDocument();
