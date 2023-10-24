76333a9095e7b2f0728799c9f1d53ea9bb3fa12c
Tristan Nigos <tristanigos@gmail.com>
2023-10-24 14:24:34 -0500

    add drawer functionality

--- src/components/PreviewComponent/PreviewComponent.tsx
+++ src/components/PreviewComponent/PreviewComponent.tsx
@@ -1,4 +1,4 @@
-import { Typography } from '@material-ui/core';
+import { Drawer, Typography, Box, Container } from '@material-ui/core';
 import React from 'react';
 
 export type QueryType = {
@@ -8,23 +8,52 @@ export type QueryType = {
   requested_reviewers: Array<any>;
 };
 
-export const PreviewComponent = ({ query }: { query?: QueryType }) => {
+export const PreviewComponent = ({
+  query,
+  state,
+  onState,
+}: {
+  query?: QueryType;
+  state: boolean;
+  onState: any;
+}) => {
   console.log('OKAY', query);
+
+  const toggleDrawer = (open: boolean) => (event: React.KeyboardEvent | React.MouseEvent) => {
+    if (
+      event.type === 'keydown' &&
+      ((event as React.KeyboardEvent).key === 'Tab' ||
+        (event as React.KeyboardEvent).key === 'Shift')
+    ) {
+      return;
+    }
+
+    onState(open);
+  };
 
   return query ? (
     <>
-      <Typography variant="h5">{query.title}</Typography>
-      <div>{query.body}</div>
+      <Drawer
+        anchor="right"
+        open={state}
+        onClose={toggleDrawer(false)}
+        BackdropProps={{ invisible: true }}
+      >
+        <Box sx={{ width: 500 }}>
+          <Typography variant="h5">{query.title}</Typography>
+          <div>{query.body}</div>
 
-      <Typography variant="h6">Assignees</Typography>
-      {query.assignees.map((item: any) => (
-        <div>{item.login}</div>
-      ))}
+          <Typography variant="h6">Assignees</Typography>
+          {query.assignees.map((item: any) => (
+            <div>{item.login}</div>
+          ))}
 
-      <Typography variant="h6">Requested Reviewers</Typography>
-      {query.requested_reviewers.map((item: any) => (
-        <div>{item.login}</div>
-      ))}
+          <Typography variant="h6">Requested Reviewers</Typography>
+          {query.requested_reviewers.map((item: any) => (
+            <div>{item.login}</div>
+          ))}
+        </Box>
+      </Drawer>
     </>
   ) : (
     <></>
