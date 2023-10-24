e158ad8be6285520052fc897db38e5dea2721062
Tristan Nigos <tristanigos@gmail.com>
2023-10-24 12:16:24 -0500

    changing props syntax

--- src/components/CardComponent/CardComponent.tsx
+++ src/components/CardComponent/CardComponent.tsx
@@ -2,16 +2,25 @@ import React from 'react';
 import { Typography, Chip } from '@material-ui/core';
 import { InfoCard } from '@backstage/core-components';
 
-export const CardComponent = ({ title, desc }: { title: string; desc: string }) => (
-  <InfoCard
-    title={title}
-    subheader={
-      <>
-        <Chip label="Chip Filled" color="primary" />
-        <Chip label="Chip Filled" color="primary" />
-      </>
-    }
-  >
-    <Typography variant="body1">{desc}</Typography>
-  </InfoCard>
-);
+export const CardComponent = ({ data, key, onQuery }: { data: any; key: any; onQuery: any }) => {
+  function handleClick() {
+    console.log(data);
+    onQuery(data);
+  }
+
+  return (
+    <div style={{ paddingBottom: '10px', cursor: 'pointer' }} onClick={handleClick}>
+      <InfoCard
+        title={data.title}
+        subheader={
+          <>
+            <Chip label="Chip Filled" color="primary" />
+            <Chip label="Chip Filled" color="primary" />
+          </>
+        }
+      >
+        <Typography variant="body1">{data.description.substring(0, 120) + '...'}</Typography>
+      </InfoCard>
+    </div>
+  );
+};
