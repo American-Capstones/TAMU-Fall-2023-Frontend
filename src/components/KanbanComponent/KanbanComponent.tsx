1da7ed77d8f75c10c1492fd9da9843e5c98b2249
Tristan Nigos <tristanigos@gmail.com>
2023-10-31 13:19:46 -0500

    initial work on team/repo sidebar

--- src/components/KanbanComponent/KanbanComponent.tsx
+++ src/components/KanbanComponent/KanbanComponent.tsx
@@ -1,5 +1,18 @@
 import React, { useState } from 'react';
-import { Grid, Container, Typography, Divider, Box, Chip } from '@material-ui/core';
+import {
+  Grid,
+  Container,
+  Typography,
+  Divider,
+  Box,
+  Chip,
+  Drawer,
+  List,
+  ListItem,
+  ListItemText,
+  Card,
+  CardContent,
+} from '@material-ui/core';
 import {
   Header,
   Page,
@@ -7,7 +20,9 @@ import {
   ContentHeader,
   HeaderLabel,
   SupportButton,
+  InfoCard,
 } from '@backstage/core-components';
+
 // import { ExampleFetchComponent } from '../ExampleFetchComponent';
 import { CardComponent } from '../CardComponent';
 import json1 from '../../res/PRS_DEFINED';
@@ -24,10 +39,10 @@ export const KanbanComponent = () => {
       <>
         <Box mb={2} m={4}>
           <Grid container spacing={2}>
-            <Grid item xs={8}>
+            <Grid item xs={10}>
               <Typography variant="h6">{columnName}</Typography>
             </Grid>
-            <Grid item xs={4}>
+            <Grid item xs={2}>
               <Chip label={columnLength} />
             </Grid>
           </Grid>
@@ -54,6 +69,51 @@ export const KanbanComponent = () => {
     );
   }
 
+  function KanbanTeams() {
+    return (
+      <>
+        <Box>
+          <Typography variant="h5">Teams</Typography>
+        </Box>
+        <List disablePadding>
+          <ListItem alignItems="flex-start" disableGutters>
+            <ListItemText
+              primary={
+                <Card>
+                  <CardContent>
+                    <Typography variant="body2">Team Repository 1</Typography>
+                  </CardContent>
+                </Card>
+              }
+            />
+          </ListItem>
+          <ListItem alignItems="flex-start" disableGutters>
+            <ListItemText
+              primary={
+                <Card>
+                  <CardContent>
+                    <Typography variant="body2">Team Repository 1</Typography>
+                  </CardContent>
+                </Card>
+              }
+            />
+          </ListItem>
+          <ListItem alignItems="flex-start" disableGutters>
+            <ListItemText
+              primary={
+                <Card>
+                  <CardContent>
+                    <Typography variant="body2">Team Repository 1</Typography>
+                  </CardContent>
+                </Card>
+              }
+            />
+          </ListItem>
+        </List>
+      </>
+    );
+  }
+
   return (
     <Page themeId="documentation">
       <Header title="Welcome to tamu-fall-2023-frontend!" subtitle="Optional subtitle">
@@ -61,25 +121,44 @@ export const KanbanComponent = () => {
         <HeaderLabel label="Lifecycle" value="Alpha" />
       </Header>
       <Content>
-        <ContentHeader title="Plugin title">
-          <SupportButton>A description of your plugin goes here.</SupportButton>
-        </ContentHeader>
-        <Grid container spacing={3} direction="row">
-          <Grid item xs={4}>
-            {KanbanColumnHeader('Defined', json1.length)}
-            {KanbanColumnBody(json1)}
+        <Grid container spacing={2} direction="row">
+          <Grid item xs={2}>
+            <Box
+              sx={{
+                display: 'flex',
+                alignItems: 'flex-start',
+                height: '100%',
+                width: 'inherit',
+              }}
+            >
+              <Container disableGutters>
+                <Box mr={2}>{KanbanTeams()}</Box>
+              </Container>
+              <Divider orientation="vertical" flexItem />
+            </Box>
           </Grid>
-          <Grid item xs={4}>
-            {KanbanColumnHeader('In Progress', json2.length)}
-            {KanbanColumnBody(json2)}
-          </Grid>
-          <Grid item xs={4}>
-            {KanbanColumnHeader('Done', json3.length)}
-            {KanbanColumnBody(json3)}
-          </Grid>
-          {/* <Grid item>
+          <Grid item xs={10}>
+            <ContentHeader title="Plugin title">
+              <SupportButton>A description of your plugin goes here.</SupportButton>
+            </ContentHeader>
+            <Grid container spacing={3} direction="row">
+              <Grid item xs={4}>
+                {KanbanColumnHeader('Defined', json1.length)}
+                {KanbanColumnBody(json1)}
+              </Grid>
+              <Grid item xs={4}>
+                {KanbanColumnHeader('In Progress', json2.length)}
+                {KanbanColumnBody(json2)}
+              </Grid>
+              <Grid item xs={4}>
+                {KanbanColumnHeader('Done', json3.length)}
+                {KanbanColumnBody(json3)}
+              </Grid>
+              {/* <Grid item>
           <ExampleFetchComponent />
         </Grid> */}
+            </Grid>
+          </Grid>
         </Grid>
         <Container maxWidth="sm">
           <PreviewComponent query={query} state={state} onState={setState} />
