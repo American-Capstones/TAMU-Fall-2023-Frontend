9c682afee46eb1cf162d48605bdf80a861a6320f
Tristan Nigos <tristanigos@gmail.com>
2023-10-31 09:51:47 -0500

    adjust spacing, add labels

--- src/components/PreviewComponent/PreviewComponent.tsx
+++ src/components/PreviewComponent/PreviewComponent.tsx
@@ -1,4 +1,4 @@
-import { Drawer, Typography, Box, Container } from '@material-ui/core';
+import { Drawer, Typography, Box, Container, Chip } from '@material-ui/core';
 import React from 'react';
 
 export type QueryType = {
@@ -6,6 +6,7 @@ export type QueryType = {
   body: string | null;
   assignees: Array<any>;
   requested_reviewers: Array<any>;
+  labels: Array<string>;
 };
 
 export const PreviewComponent = ({
@@ -20,6 +21,7 @@ export const PreviewComponent = ({
   console.log('OKAY', query);
 
   const toggleDrawer = (open: boolean) => (event: React.KeyboardEvent | React.MouseEvent) => {
+    console.log(query);
     if (
       event.type === 'keydown' &&
       ((event as React.KeyboardEvent).key === 'Tab' ||
@@ -39,19 +41,32 @@ export const PreviewComponent = ({
         onClose={toggleDrawer(false)}
         BackdropProps={{ invisible: true }}
       >
-        <Box sx={{ width: 500 }}>
-          <Typography variant="h5">{query.title}</Typography>
-          <div>{query.body}</div>
+        <Box width={350} m={2} mt={8} alignItems="center" justifyContent="center">
+          <Box mt={2}>
+            <Typography variant="h4">{query.title}</Typography>
+            <div>{query.body}</div>
+          </Box>
 
-          <Typography variant="h6">Assignees</Typography>
-          {query.assignees.map((item: any) => (
-            <div>{item.login}</div>
-          ))}
+          <Box mt={2}>
+            <Typography variant="h6">Assignees</Typography>
+            {query.assignees.map((item: any) => (
+              <div>{item.login}</div>
+            ))}
+          </Box>
 
-          <Typography variant="h6">Requested Reviewers</Typography>
-          {query.requested_reviewers.map((item: any) => (
-            <div>{item.login}</div>
-          ))}
+          <Box mt={2}>
+            <Typography variant="h6">Requested Reviewers</Typography>
+            {query.requested_reviewers.map((item: any) => (
+              <div>{item.login}</div>
+            ))}
+          </Box>
+
+          <Box mt={2}>
+            <Typography variant="h6">Labels</Typography>
+            {query.labels.map((item: any) => (
+              <Chip label={item}></Chip>
+            ))}
+          </Box>
         </Box>
       </Drawer>
     </>
