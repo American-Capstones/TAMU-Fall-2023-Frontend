d8d351bb85e98e2c95b290ad0aa0c12253c2325c
Tristan Nigos <tristanigos@gmail.com>
2023-11-16 15:33:01 -0600

    add interfaces that match endpoint, handle current repo kanban view, get all repos for initial pop

--- src/components/KanbanComponent/KanbanComponent.tsx
+++ src/components/KanbanComponent/KanbanComponent.tsx
@@ -1,18 +1,5 @@
-import React, { useState } from 'react';
-import {
-  Grid,
-  Container,
-  Typography,
-  Divider,
-  Box,
-  Chip,
-  Drawer,
-  List,
-  ListItem,
-  ListItemText,
-  Card,
-  CardContent,
-} from '@material-ui/core';
+import React, { useEffect, useState } from 'react';
+import { Grid, Container } from '@material-ui/core';
 import {
   Header,
   Page,
@@ -26,28 +13,124 @@ import { CardComponent } from '../CardComponent';
 import DEFINED_PRs from '../../res/PRS_DEFINED';
 import IN_PROGRESS_PRs from '../../res/PRS_INPROGRESS';
 import DONE_PRs from '../../res/PRS_DONE';
-import { PreviewComponent, QueryType } from '../PreviewComponent/PreviewComponent';
+import { PreviewComponent } from '../PreviewComponent/PreviewComponent';
 import { KanbanColumnHeader } from './KanbanColumnComponent/KanbanColumnHeaderComponent';
 import { KanbanTeamsComponent } from './KanbanTeamsComponent';
 
-export const KanbanComponent = () => {
-  const [query, setQuery] = useState<QueryType>();
-  const [sideDrawOpen, setSideDrawOpen] = useState(false);
+import { githubAuthApiRef, useApi } from '@backstage/core-plugin-api';
 
-  const KanbanColumnBody = (pullRequests: any[]) => {
+export type CommentType = {
+  author: {
+    login: string;
+  };
+  body: string;
+  createdAt: string;
+  updatedAt: string;
+};
+
+export type ReviewType = {
+  author: {
+    login: string;
+  };
+  body: string;
+  comments: {
+    nodes: CommentType[];
+  };
+  createdAt: string;
+  state: string;
+  updatedAt: string;
+};
+
+export type LabelType = {
+  color: string;
+  name: string;
+};
+
+export type PRType = {
+  author: {
+    login: string;
+  };
+  body: string;
+  createdAt: string;
+  number: number;
+  labels: {
+    nodes: LabelType[];
+  };
+  reviews: {
+    nodes: ReviewType[];
+  };
+  state: string;
+  title: string;
+  updatedAt: string;
+  url: string;
+};
+
+export type RepoType = {
+  repository: string;
+  data: PRType[];
+};
+
+export const KanbanComponent = () => {
+  const [query, setQuery] = useState<PRType>();
+  const [userRepos, setUserRepos] = useState<RepoType[]>();
+  const [userRepoNames, setUserRepoNames] = useState<String[]>();
+  const [sideDrawOpen, setSideDrawOpen] = useState(false);
+  const [userRepoView, setUserRepoView] = useState(0);
+  const [username, setUsername] = useState<string | undefined>('');
+
+  const KanbanColumnBody = (pullRequests: PRType[]) => {
+    console.log('pullrequest', pullRequests);
+
     return (
       <Container style={{ maxHeight: 700, overflow: 'auto' }}>
-        {pullRequests.map((item: any) => (
-          <CardComponent
-            data={item}
-            key={item.id}
-            onQuery={setQuery}
-            onSideDrawOpen={setSideDrawOpen}
-          />
-        ))}
+        {pullRequests &&
+          pullRequests.map((item: PRType, id: number) => (
+            <CardComponent
+              data={item}
+              key={id}
+              onQuery={setQuery}
+              onSideDrawOpen={setSideDrawOpen}
+            />
+          ))}
       </Container>
     );
   };
+
+  const requestOptions = {
+    method: 'POST',
+    headers: { 'Content-Type': 'application/json' },
+    body: '',
+  };
+
+  const ghecAuthApi = useApi(githubAuthApiRef);
+  useEffect(() => {
+    async function fetchData() {
+      const backstageUserIdentity = await ghecAuthApi.getProfile();
+      setUsername(backstageUserIdentity?.displayName);
+      requestOptions.body = JSON.stringify({ user_id: backstageUserIdentity?.displayName });
+
+      console.log('reqoptions', requestOptions);
+      await fetch('http://localhost:7007/api/pr-tracker-backend/get-user-repos', requestOptions)
+        .then((response) => response.json())
+        .then((data) => {
+          console.log('DATA', data);
+          setUserRepos(data);
+
+          const repoNames: String[] = [];
+          data.map((repo: RepoType) => {
+            repoNames.push(repo.repository);
+          });
+
+          console.log('REPONAMES', repoNames);
+
+          setUserRepoNames(repoNames);
+        });
+
+      console.log(3);
+    }
+    fetchData();
+    console.log('USER REPOS', userRepos);
+  }, []);
 
   return (
     <Page themeId="documentation">
@@ -57,7 +140,16 @@ export const KanbanComponent = () => {
       </Header>
       <Content>
         <Grid container spacing={2} direction="row">
-          <KanbanTeamsComponent />
+          {userRepos ? (
+            <KanbanTeamsComponent
+              userRepoNames={userRepoNames}
+              setUserRepoView={setUserRepoView}
+              username={username}
+            />
+          ) : (
+            <></>
+          )}
+
           <Grid item xs={10}>
             <ContentHeader title="American Airlines PR Board">
               <SupportButton>A description of your plugin goes here.</SupportButton>
@@ -65,15 +157,15 @@ export const KanbanComponent = () => {
             <Grid container spacing={3} direction="row">
               <Grid item xs={4}>
                 {KanbanColumnHeader('Defined', DEFINED_PRs.length)}
-                {KanbanColumnBody(DEFINED_PRs)}
+                {userRepos && KanbanColumnBody(userRepos[userRepoView]?.data)}
               </Grid>
               <Grid item xs={4}>
                 {KanbanColumnHeader('In Progress', IN_PROGRESS_PRs.length)}
-                {KanbanColumnBody(IN_PROGRESS_PRs)}
+                {/* {KanbanColumnBody(IN_PROGRESS_PRs)} */}
               </Grid>
               <Grid item xs={4}>
                 {KanbanColumnHeader('Done', DONE_PRs.length)}
-                {KanbanColumnBody(DONE_PRs)}
+                {/* {KanbanColumnBody(DONE_PRs)} */}
               </Grid>
               {/* <Grid item>
           <ExampleFetchComponent />
