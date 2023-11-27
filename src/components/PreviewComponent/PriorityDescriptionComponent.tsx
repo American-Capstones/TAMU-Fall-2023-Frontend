8a0a46933d405f6dddc3c372652879cd5d915e19
Tristan Nigos <tristanigos@gmail.com>
2023-11-27 11:56:36 -0600

    init

---
+++ src/components/PreviewComponent/PriorityDescriptionComponent.tsx
@@ -0,0 +1,7 @@
+import React from 'react';
+import { InputLabel, Select, MenuItem, FormControl, makeStyles } from '@material-ui/core';
+import { configApiRef, useApi } from '@backstage/core-plugin-api';
+
+export const PriorityDescriptionComponent = () => {
+  return <></>;
+};
