3de660b62b1dc6f5ae923bcc074f1f239951f7f0
Tristan Nigos <tristanigos@gmail.com>
2023-11-28 04:20:19 -0600

    add app-config visibility

---
+++ config.d.ts
@@ -0,0 +1,15 @@
+export interface Config {
+  'pr-tracker-backend': {
+    /**
+     * Frontend root URL
+     * @visibility frontend
+     */
+    baseUrl: string;
+
+    /**
+     * Frontend root URL
+     * @visibility frontend
+     */
+    organization: string;
+  };
+}
